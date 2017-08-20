fe2d36b5d1926ad6b14fe981c17d9fa699bccc2e
Kyle Steinle <kyledavid022@gmail.com>
2017-08-20 11:29:38 -0700

    toggled display for fire images instead of reloading each time a dish is cooked

--- src/components/Cook.jsx
+++ src/components/Cook.jsx
@@ -1,22 +1,26 @@
 const React = require('react');
 const PropTypes = require('prop-types');
 
-class Cook extends React.Component {
-  render() {
-    const fire = this.props.cooked ? require('../img/fire-burning.png') : require('../img/fire-out.png');
-    return (
-      <section id='cook' className="component cook-comp">
-        <button
-          onClick={this.props.cook}
-          className="btn-cook"
-          >COOK</button>
-          <img
-            style={{width: '40%', margin: 'auto', display: 'block', marginTop: '40px'}}
-            src={fire}
-          />
-      </section>
-    )
-  }
+const Cook = (props) => {
+  const sticks = require('../img/fire-out.png');
+  const fire = require('../img/fire-burning.png');
+
+  return (
+    <section id='cook' className="component cook-comp">
+      <button
+        onClick={props.cook}
+        className="btn-cook"
+        >COOK</button>
+        <img
+          style={{width: '40%', margin: 'auto', display: props.cooked ? 'none' : 'block', marginTop: '40px'}}
+          src={sticks}
+        />
+        <img
+          style={{width: '40%', margin: 'auto', display: props.cooked ? 'block' : 'none', marginTop: '40px'}}
+          src={fire}
+        />
+    </section>
+  );
 }
 
 Cook.propTypes = {
